94fb4a32fcbb601781842e95b8af3420bf7316fc
akash1551 <acwankhede@gmail.com>
2017-10-08 15:58:31 +0530

    resolved bill add bug

--- static/apps/adminDashboard/user/addBillCtrl.js
+++ static/apps/adminDashboard/user/addBillCtrl.js
@@ -5,6 +5,20 @@ angular.module('controllers')
 	$scope.emailFormat = /^[a-z]+[a-z0-9._]+@[a-z]+\.[a-z.]{2,5}$/;
 
 	$scope.initializeBillDetails = function () {
+		// 	$scope.billDetails = {
+		// 	bill_no: '859',
+		// 	op_no: '58746',
+		// 	token: 'asds78',
+		// 	patient_name: 'Test Patient',
+		// 	consultant: 'Test Consultant',
+		// 	perticulars: 'Test Perticulars',
+		// 	qty: '25',
+		// 	patient_age: '25',
+		// 	mobile_no: '7896541230',
+		// 	gender: '1',
+		// 	payment_type: '1'
+		// }
+
 		$scope.billDetails = {
 			bill_no: null,
 			op_no: null,
@@ -18,6 +32,7 @@ angular.module('controllers')
 			gender: null,
 			payment_type: null
 		}
+
 	}
 
 	$timeout(function(){
@@ -25,6 +40,7 @@ angular.module('controllers')
 	});
 
 	$scope.addBill = function() {
+
 		console.log('Bill', $scope.billDetails);
 		var dataPromis = networkCall.addBillRequest($scope.billDetails);
 		dataPromis.then(function(result) {
